a625ce8071176179e7549629072276202b1a2894
Banana0910 <dleogus0910@gmail.com>
2022-01-28 19:45:10 +0900

    2022/01/28 수정

--- command.js
+++ command.js
@@ -2,85 +2,106 @@ const { Client } = require("discord-slash-commands-client");
 const { token, client_id } = require("./config.json")
 
 const client = new Client(token, client_id);
-const guild_id = "758758978078769233"
+const guild_id = "783625320062386217"
+
+// client.createCommand({
+//     name: "eval",
+//     description: "javascript 명령을 실행합니다.",
+//     options: [
+//         {
+//             name: "명령어",
+//             description: "실행할 명령어를 입력하면 됩니다.",
+//             required: true,
+//             type: 3,
+//         }
+//     ]
+// }, guild_id).then(console.log);
+
+// client.createCommand({
+//     name: "getinf",
+//     description: "급식 정보를 가져옵니다.",
+// }, guild_id).then(console.log);
+
+// client.createCommand({
+//     name: "show",
+//     description: "급식 정보를 보여줍니다.",
+// }, guild_id).then(console.log);
+
+// client.createCommand({
+//     name: "schoolmeal",
+//     description: "특정 일의 급식을 가져오고, 보여줍니다.",
+//     options: [
+//         {
+//             name: "연",
+//             description: "가져올 급식의 연도",
+//             required: true,
+//             type: 4,
+//             min_value: 1,
+//             max_value: 2022,
+//         },
+//         {
+//             name: "월",
+//             description: "가져올 급식의 월",
+//             required: true,
+//             type: 4,
+//             min_value: 1,
+//             max_value: 12,
+//         },
+//         {
+//             name: "일",
+//             description: "가져올 급식의 일",
+//             required: true,
+//             type: 4,
+//             min_value: 1,
+//             max_value: 31,
+//         }
+//     ]
+// }, guild_id).then(console.log);
+
+// client.createCommand({
+//     name: "set",
+//     description: "타겟 채널을 설정합니다.",
+//     options: [
+//         {
+//             name: "채널",
+//             description: "실행할 채널의 ID를 입력하시면 됩니다.",
+//             required: false,
+//             type: 7,
+//             channel_types: 0,
+//         }
+//     ]
+// }, guild_id).then(console.log);
 
 client.createCommand({
-    name: "eval",
-    description: "javascript 명령을 실행합니다.",
+    name: "play",
+    description: "youtube에서 원하는 영상의 오디오를 재생합니다.",
     options: [
         {
-            name: "명령어",
-            description: "실행할 명령어를 입력하면 됩니다.",
+            name: "url",
+            description: "원하는 youtube영상의 URL을 입력합니다.",
             required: true,
             type: 3,
-        }
-    ]
-}, guild_id).then(console.log);
-
-client.createCommand({
-    name: "getinf",
-    description: "급식 정보를 가져옵니다.",
-}, guild_id).then(console.log);
-
-client.createCommand({
-    name: "show",
-    description: "급식 정보를 보여줍니다.",
-}, guild_id).then(console.log);
-
-client.createCommand({
-    name: "schoolmeal",
-    description: "특정 일의 급식을 가져오고, 보여줍니다.",
-    options: [
-        {
-            name: "연",
-            description: "가져올 급식의 연도",
-            required: true,
-            type: 4,
-            min_value: 1,
-            max_value: 2022,
         },
         {
-            name: "월",
-            description: "가져올 급식의 월",
-            required: true,
-            type: 4,
-            min_value: 1,
-            max_value: 12,
-        },
-        {
-            name: "일",
-            description: "가져올 급식의 일",
-            required: true,
-            type: 4,
-            min_value: 1,
-            max_value: 31,
-        }
-    ]
-}, guild_id).then(console.log);
-
-client.createCommand({
-    name: "set",
-    description: "타겟 채널을 설정합니다.",
-    options: [
-        {
-            name: "채널",
-            description: "실행할 채널의 ID를 입력하시면 됩니다.",
+            name: "음량",
+            description: "원하는 음량의 값을 정합니다.",
             required: false,
-            type: 7,
-            channel_types: 0,
+            type: 4,
+            min_value: 0,
+            max_value: 10,
         }
     ]
 }, guild_id).then(console.log);
 
 client.createCommand({
-    name: "sendmsg",
-    description: "타겟 채널에 메시지를 전송합니다.",
+    name: "clean",
+    description: "정해진 횟수만큼 씨부린것들을 삭제합니다.",
     options: [
         {
-            name: "메시지",
-            description: "보낼 메시지를 입력합니다",
+            name: "양",
+            description: "삭제하고싶은 양",
             required: true,
-            type: 3,
+            type: 4,
         }
     ]
 }, guild_id).then(console.log);
--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed, Message, MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
+const { createAudioPlayer, createAudioResource, AudioPlayerStatus, joinVoiceChannel } = require('@discordjs/voice');
 const { token } = require('./config.json');
 
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const bot = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+    ] 
+});
 
-let today_lunch = { 
-    meal: "",
-    calorie: ""
-}
-let today_dinner = {
-    meal: "",
-    calorie: "",
+let today_lunch = { meal: "", calorie: "" }
+let today_dinner = { meal: "", calorie: "" }
+
+let playlist = {};
+
+async function play_embed(interaction, info, volume, loop, btn) {
+    await interaction.editReply({ 
+        embeds: [ new MessageEmbed({
+            title: info.videoDetails.title,
+            image: { url: info.videoDetails.thumbnails[3].url },
+            color: "#ff0000",
+            url: info.videoDetails.video_url,
+            author: { name: info.videoDetails.ownerChannelName },
+            description: `음량 : ${Math.floor(volume*10)} ${(loop == true) ? "| 루프" : ""}`,
+        })],
+        components: [btn]
+    });
 }
 
-let target_channel = undefined;
-
-function embed(title, descirption) {
-    const { MessageEmbed } = require('discord.js');
-    return new MessageEmbed()
-        .setTitle(title)
-        .setDescription(descirption)
-        .setColor("0x139BCC");
-}
 
 async function getinformation() {
     const che = require('cheerio');
@@ -37,7 +45,10 @@ async function getinformation() {
         const temp = $("#subContent > div > div:nth-child(7) > div:nth-child(6) > table > tbody > tr:nth-child(2) > td");
         if (temp.length) {
             today_dinner = {
-                meal: temp.html().replace(/<br\s*[\/]?>/gi, '\n').replace(/[0-9\\.]/gi, '').trim(),
+                meal: temp.html().trim()        
+                .replace(/\n/gi, '')
+                .replace(/<br\s*[\/]?>/gi, '\n')
+                .replace(/[0-9\\.]/gi, ''),
                 calorie: $("#subContent > div > div:nth-child(7) > div:nth-child(6) > table > tbody > tr:nth-child(4) > td").text().trim()
             }
         }
@@ -58,7 +69,10 @@ async function getschoolmeal(year,month,day) {
         const $ = che.load(html.data);
         
         lunch = {
-            meal: $("#subContent > div > div:nth-child(7) > div:nth-child(5) > table > tbody > tr:nth-child(2) > td").html().trim().replace(/\n/gi, '').replace(/<br\s*[\/]?>/gi, '\n').replace(/[0-9\\.]/gi, ''),
+            meal: $("#subContent > div > div:nth-child(7) > div:nth-child(5) > table > tbody > tr:nth-child(2) > td").html().trim()
+                .replace(/\n/gi, '')
+                .replace(/<br\s*[\/]?>/gi, '\n')
+                .replace(/[0-9\\.]/gi, ''),
             calorie: $("#subContent > div > div:nth-child(7) > div:nth-child(5) > table > tbody > tr:nth-child(4) > td").text().trim()
         }
         const temp = $("#subContent > div > div:nth-child(7) > div:nth-child(6) > table > tbody > tr:nth-child(2) > td");
@@ -133,19 +147,124 @@ bot.on('interactionCreate', async interaction => {
         } else {
             await interaction.editReply(`${year}년 ${month}월 ${day}일에는 급식이 없네요..`);
         }
+    } else if (interaction.commandName === 'play') {
+        const user_channel = interaction.guild.members.cache.get(interaction.user.id).voice.channel;
+        if (!user_channel) {
+            await interaction.reply("먼저 채널에 입장해주세요");
+            return;
+        }
+
+        const ytdl = require('ytdl-core');
+        const url = interaction.options.getString("url");
+        let vid_info = undefined;
+        await interaction.deferReply();
+        await ytdl.getInfo(url).then(async info => {
+            vid_info = info;  
+        }).catch(async e => {
+            await interaction.editReply("음.. 올바른 유튜브 링크가 아닌듯하네요");
+            return;
+        })
+
+        let volume = interaction.options.getInteger("음량");
+        volume = (volume) ? volume/10 : 1.0
+        if (!playlist[interaction.guild.id]) {
+            playlist[interaction.guild.id] = [];
+            playlist[interaction.guild.id].push({ info: vid_info, stream: ytdl(url, { filter: 'audioonly', highWaterMark: 1 << 25 }) });
+        } else {
+            playlist[interaction.guild.id].push({ info: vid_info, stream: ytdl(url, { filter: 'audioonly', highWaterMark: 1 << 25 }) });
+            await interaction.editReply(`**${vid_info.videoDetails.title}** (이)가 리스트에 추가됨`);
+            return;
+        }
+
+        let btn = new MessageActionRow({
+            components: [
+                new MessageButton({ customId: "stop", label: "X", style: 'DANGER' }), 
+                new MessageButton({ customId: "loop", emoji: "♻️", style: 'PRIMARY' }), 
+                new MessageButton({ customId: "vol_up", emoji: "⬆️", style: 'SECONDARY', disabled: (volume < 1.0) ? false : true }), 
+                new MessageButton({ customId: "vol_down", emoji: "⬇️", style: 'SECONDARY', disabled: (volume > 0.1) ? false : true }), 
+                new MessageButton({ customId: "skip", emoji: "➡️", style: 'SUCCESS'})
+            ]
+        });
+        const connection = joinVoiceChannel({
+            channelId: user_channel.id,
+            guildId: user_channel.guild.id,
+            adapterCreator: user_channel.guild.voiceAdapterCreator,
+        });
+        let loop = false;
+        
+        await play_embed(interaction, vid_info, volume, loop, btn);
+        let resource = createAudioResource(playlist[interaction.guild.id][0].stream, { inlineVolume: true });
+        const player = createAudioPlayer();
+        player.play(resource);
+        connection.subscribe(player);
+
+        // 리스너
+        const collecter = interaction.channel.createMessageComponentCollector();
+        collecter.on('collect', async i => {
+            i.deferUpdate();
+            if (i.customId === "vol_up") {
+                volume += 0.1
+                resource.volume.setVolume(volume)
+            } else if (i.customId === "vol_down") {
+                volume -= 0.1
+                resource.volume.setVolume(volume)
+            } else if (i.customId === "stop") {
+                playlist[interaction.guild.id] = undefined;
+                player.stop();
+                collecter.stop();
+                connection.disconnect();
+                connection.destroy();
+                interaction.deleteReply();
+                return;
+            } else if (i.customId === "loop") {
+                loop = (loop == true) ? false : true;
+            } else if (i.customId === "skip") {
+                player.stop();
+            }
+            btn.components[2].setDisabled((volume < 1.0) ? false : true)
+            btn.components[3].setDisabled((volume > 0.0) ? false : true)
+            const info = playlist[interaction.guild.id][0].info;
+            await play_embed(interaction, info, volume, loop, btn);
+        });
+        player.on(AudioPlayerStatus.Idle, async () => {
+            if (loop == true) {
+                resource = createAudioResource(ytdl(
+                    playlist[interaction.guild.id][0].info.videoDetails.video_url, 
+                    { filter: 'audioonly', highWaterMark: 1 << 25 }), 
+                    { inlineVolume: true });
+                player.play(resource);
+                return;
+            }
+            if (playlist[interaction.guild.id].length == 1) {
+                playlist[interaction.guild.id] = undefined;
+                player.stop();
+                collecter.stop();
+                connection.disconnect();
+                connection.destroy();
+                interaction.deleteReply();
+            } else {
+                playlist[interaction.guild.id].shift();
+                const vid = playlist[interaction.guild.id][0];
+                resource = createAudioResource(vid.stream, { inlineVolume: true });
+                player.play(resource);
+                const info = vid.info
+                await play_embed(interaction, info, volume, loop, btn);
+            }
+        });
+
+    } else if (interaction.commandName === 'clean') {
+        const count = interaction.options.getInteger("양");
+        await interaction.channel.bulkDelete(count);
+        await interaction.reply(`${count}만큼의 채팅을 삭제했음!`);
     }
 });
 
-const activity_list = ["테스트", "자살"];
-let turn = 0
+const activity_list = ["테스트", "디버깅", "수리"];
+let turn = 0    
 
 function chanege_activity() {
     bot.user.setActivity(activity_list[turn], { type: "PLAYING" });
-    if (turn == 1) {
-        turn = 0;
-    } else {
-        turn = 1;
-    }
+    turn = (turn == 0) ? 1 : 0;
 }
 
 bot.login(token);
