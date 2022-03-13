c37551a950ddbb0ab69a3e8c8aa60e4d6130b3ad
Banana0910 <dleogus0910@gmail.com>
2022-03-14 00:32:17 +0900

    03/14 commit

--- src/commands/show.js
+++ src/commands/show.js
@@ -7,8 +7,8 @@ module.exports = {
      * @param {CommandInteraction} interaction
      */
     async execute(interaction) {
+        let data = require('../api/school/meal_data.json');
         const color = interaction.guild.me.displayHexColor
-        const data = require('../api/school/meal_data.json');
         const target = (interaction.options.getString("날") == "today") ? data.today : data.nextday
         await interaction.reply({ embeds: [ meal_embed(target, color), timetable_embed(target, color)] });
     }
